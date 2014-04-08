ab25a583d75d483430e4cbd7020d8b5ed934696e
ephys <ephyspotato@gmail.com>
2014-04-08 20:15:16 +0200

    Potion diffuser completed !

--- minecraft/nf/fr/ephys/playerproxies/common/tileentity/TileEntityPotionDiffuser.java
+++ minecraft/nf/fr/ephys/playerproxies/common/tileentity/TileEntityPotionDiffuser.java
@@ -1,5 +1,6 @@
 package nf.fr.ephys.playerproxies.common.tileentity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.block.Block;
@@ -7,7 +8,10 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPotion;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
@@ -17,12 +21,21 @@ import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.FluidTankInfo;
 import net.minecraftforge.fluids.IFluidHandler;
+import nf.fr.ephys.playerproxies.common.PlayerProxies;
+import nf.fr.ephys.playerproxies.helpers.NBTHelper;
 import nf.fr.ephys.playerproxies.util.cofh.TileEnergyHandler;
 
 public class TileEntityPotionDiffuser extends TileEnergyHandler implements ISidedInventory, IFluidHandler {
 	private FluidTank tank = new FluidTank(20000);
+
+	private List<PotionEffect> potionEffects = new ArrayList<PotionEffect>();
 	
-	public static final int RANGE = 16;
+	private ItemStack[] inventorySlots = new ItemStack[2];
+	
+	public static final int SLOT_POTION = 1;
+	public static final int SLOT_BOTTLE = 0;
+
+	public static final int RANGE = 8;
 
 	@Override
 	public int fill(ForgeDirection from, FluidStack resource, boolean doFill) {
@@ -59,116 +72,163 @@ public class TileEntityPotionDiffuser extends TileEnergyHandler implements ISide
 		return new FluidTankInfo[] { this.tank.getInfo() };
 	}
 	
+	private static final int INTERVAL = 20;
+	private int tick = INTERVAL;
+	
 	@Override
 	public void updateEntity() {
-		System.out.println(this.tank.getFluidAmount());
-		if (this.tank.getFluidAmount() >= 1000) {
+		if (tick-- == 0) {
+			tick = INTERVAL;
+
 			List<EntityLivingBase> entities = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(
 				this.xCoord - RANGE, this.yCoord - RANGE, this.zCoord - RANGE, 
 				this.xCoord + RANGE, this.yCoord + RANGE, this.zCoord + RANGE
 			));
-			
-			for (EntityLivingBase entity : entities) {
-				Block block = Block.blocksList[this.tank.getFluid().getFluid().getBlockID()];
 
-				block.onEntityCollidedWithBlock(worldObj, xCoord, yCoord, zCoord, entity);
-				block.onEntityWalking(worldObj, xCoord, yCoord, zCoord, entity);
+			if (this.tank.getFluidAmount() >= INTERVAL) {
+				for (EntityLivingBase entity : entities) {
+					Block block = Block.blocksList[this.tank.getFluid().getFluid().getBlockID()];
+	
+					block.onEntityCollidedWithBlock(worldObj, xCoord, yCoord, zCoord, entity);
+					block.onEntityWalking(worldObj, xCoord, yCoord, zCoord, entity);
+				}
+	
+				this.tank.drain(INTERVAL, true);
 			}
-			
-			this.tank.drain(1000, true);
+
+			if (this.potionEffects.isEmpty() && this.inventorySlots[SLOT_POTION] != null && this.inventorySlots[SLOT_BOTTLE] == null) {
+				ItemPotion potion = (ItemPotion) this.inventorySlots[SLOT_POTION].getItem();
+				
+				this.potionEffects = potion.getEffects(this.inventorySlots[SLOT_POTION]);
+				
+				this.setInventorySlotContents(SLOT_POTION, null);
+				
+				this.setInventorySlotContents(SLOT_BOTTLE, new ItemStack(Item.glassBottle));
+			}
+
+			// activate potion effect
+			for (int i = 0; i < potionEffects.size(); i++) {
+				PotionEffect potionEffect = potionEffects.get(i);
+				PotionEffect activePotion = new PotionEffect(potionEffect);
+
+				int duration = Math.min(potionEffect.duration, INTERVAL+5);
+
+				activePotion.duration = duration;
+				potionEffect.duration -= duration;
+
+				if (potionEffect.duration <= 0)
+					potionEffects.remove(i);
+
+				for (EntityLivingBase entity : entities) {
+					entity.addPotionEffect(activePotion);
+				}
+			}
 		}
-		
+
 		super.updateEntity();
 	}
 
 	@Override
 	public int getSizeInventory() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 2;
 	}
 
 	@Override
 	public ItemStack getStackInSlot(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		return i < 2 ? this.inventorySlots[i] : null;
 	}
 
 	@Override
-	public ItemStack decrStackSize(int i, int j) {
-		// TODO Auto-generated method stub
-		return null;
+	public ItemStack decrStackSize(int slot, int amount) {
+		ItemStack stack = getStackInSlot(slot);
+
+		if (stack != null && amount > 0) {
+			setInventorySlotContents(slot, null);
+		}
+
+		return stack;
 	}
 
 	@Override
 	public ItemStack getStackInSlotOnClosing(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		ItemStack stack = getStackInSlot(i);
+
+		if (stack != null) {
+			setInventorySlotContents(i, null);
+		}
+
+		return stack;
 	}
 
 	@Override
-	public void setInventorySlotContents(int i, ItemStack itemstack) {
-		// TODO Auto-generated method stub
+	public void setInventorySlotContents(int i, ItemStack stack) {
+		if (!isItemValidForSlot(i, stack))
+			return;
 
+		if (stack != null) {
+			if (stack.stackSize > getInventoryStackLimit())
+				stack.stackSize = getInventoryStackLimit();
+		}
+
+		this.inventorySlots[i] = stack;
+
+		onInventoryChanged();
+	}
+	
+	@Override
+	public void onInventoryChanged() {
+		super.onInventoryChanged();
 	}
 
 	@Override
 	public String getInvName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "ephys.pp.tileEntityPotionDiffuser";
 	}
 
 	@Override
 	public boolean isInvNameLocalized() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public int getInventoryStackLimit() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 	@Override
-	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isUseableByPlayer(EntityPlayer player) {
+		return worldObj.getBlockTileEntity(xCoord, yCoord, zCoord) == this
+				&& player.getDistanceSq(xCoord + 0.5, yCoord + 0.5,
+						zCoord + 0.5) < 64;
 	}
 
 	@Override
-	public void openChest() {
-		// TODO Auto-generated method stub
+	public void openChest() {}
 
+	@Override
+	public void closeChest() {}
+
+	@Override
+	public boolean isItemValidForSlot(int slot, ItemStack itemstack) {
+		switch(slot) {
+			case SLOT_BOTTLE: return itemstack == null || (itemstack.itemID == Item.glassBottle.itemID && itemstack.stackSize == getInventoryStackLimit());
+			case SLOT_POTION: return itemstack == null || (itemstack.itemID == Item.potion.itemID && itemstack.stackSize == getInventoryStackLimit());
+			default: return false;
+		}
 	}
 
 	@Override
-	public void closeChest() {
-		// TODO Auto-generated method stub
-
+	public int[] getAccessibleSlotsFromSide(int side) {
+		return side < 2 ? new int[] { side } : new int[] {};
 	}
 
 	@Override
-	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean canInsertItem(int slot, ItemStack itemstack, int side) {
+		return side < 2 && side == slot && isItemValidForSlot(slot, itemstack);
 	}
 
 	@Override
-	public int[] getAccessibleSlotsFromSide(int var1) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean canExtractItem(int slot, ItemStack itemstack, int side) {
+		return canInsertItem(slot, itemstack, side);
 	}
-
-	@Override
-	public boolean canInsertItem(int i, ItemStack itemstack, int j) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean canExtractItem(int i, ItemStack itemstack, int j) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
