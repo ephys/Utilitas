ab994b9c4d7b99374c77fc1195ed49c4f2efc049
ephys <ephyspotato@gmail.com>
2014-07-28 18:54:22 +0200

    fixes... or not

--- main/java/nf/fr/ephys/playerproxies/common/registry/uniterface/InterfaceTileEntity.java
+++ main/java/nf/fr/ephys/playerproxies/common/registry/uniterface/InterfaceTileEntity.java
@@ -28,6 +28,9 @@ public class InterfaceTileEntity extends UniversalInterface {
 		GL11.glRotatef(tickCount, 0.0F, 1.0F, 0.0F);
 		GL11.glRotatef(-30.0F, 1.0F, 0.0F, 0.0F);
 
+		//RenderHelper.loadBlockMap();
+		//Block block = blockEntity == null ? Blocks.chest : blockEntity.getBlockType();
+		// todo: there is a lot of problems with rendering the thingy as an item, need to sort that out
 		nf.fr.ephys.playerproxies.client.renderer.TileEntityInterfaceRenderer.renderBlocksInstance.renderBlockAsItem(Blocks.chest, 0, 1.0F);
 	}
 
@@ -67,8 +70,6 @@ public class InterfaceTileEntity extends UniversalInterface {
 
 	@Override
 	public void onTick(int tick) {
-		if (getTileEntity().getWorldObj().isRemote) return;
-
 		if (blockEntity == null) {
 			if (tileLocation == null || tileLocation.length != 3) {
 				this.getTileEntity().unlink();
