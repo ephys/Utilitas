8b7154c0ac07374cb4f68cc69ebee3320815d538
Guylian Cox <gc@pictawall.com>
2017-06-09 14:39:31 +0200

    build data

--- src/main/java/be/ephys/utilitas/Utilitas.java
+++ src/main/java/be/ephys/utilitas/Utilitas.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class Utilitas {
 
-    public static final String VERSION = "1.10.2-2.0.0";
+    public static final String VERSION = "1.10.2-1.0.0";
     public static final String MODID = "utilitas";
     public static final String NAME = "Utilitas";
 
